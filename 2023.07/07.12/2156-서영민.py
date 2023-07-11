6ac5998164129341d5eab9246bda536697986eaa
Youngmanbo <yo900326@gmail.com>
2023-07-12 08:51:19 +0900

    youngmin

---
+++ 2023.07/07.12/2156-서영민.py
@@ -0,0 +1,16 @@
+n = int(input())
+wine = [int(input()) for _ in range(n)]
+
+dp = [0]*n
+
+dp[0] = wine[0]
+
+if n > 1:
+    dp[1] = wine[0]+wine[1]
+if n > 2:
+    dp[2] = max(wine[0]+wine[1], wine[1]+wine[2])
+
+for i in range(3, n):
+    dp[i] = max(dp[i-1], dp[i-2]+wine[i], dp[i-3]+wine[i-1]+wine[i])
+
+print(dp[n-1])
