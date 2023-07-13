ed474caa016f8da24dd132df117a9ce01eafc95e
Youngmanbo <yo900326@gmail.com>
2023-07-14 08:53:55 +0900

    변경

--- 2023.07/07.14/11123. 양-서영민.py
+++ 2023.07/07.14/11123. 양-서영민.py
@@ -4,16 +4,16 @@ sys.setrecursionlimit(100000)
 
 T = int(input())
 
-def dfs(x,y, h, w):
+def dfs(x,y):
     gragh[x][y] = '.'
     rows = [-1, 1, 0, 0]
     cols = [0, 0, -1, 1]
     for i in range(4):
         row = rows[i]+x
         col = cols[i]+y
-        if 0 <= row < h and 0 <= col < w:
+        if 0 <= row < H and 0 <= col < W:
             if gragh[row][col] == '#':
-                dfs(row, col, h, w)
+                dfs(row, col)
 
 for _ in range(T):
     H, W = map(int, input().split())
@@ -22,7 +22,7 @@ for _ in range(T):
     for i in range(H):
         for j in range(W):
             if gragh[i][j] == '#':
-                dfs(i, j, H, W)
+                dfs(i, j)
                 count += 1
     print(count)      
         
