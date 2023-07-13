c546b537c4d8a01a661029c62cccd097226c6c6c
RoxyYujinKim <yujinkim0707@gmail.com>
2023-07-14 08:34:17 +0900

    update: pick_presenter 중복값 나오지 않게 수정

--- pick_presenter.py
+++ pick_presenter.py
@@ -7,5 +7,6 @@ peoples = ['채홍', '성원', '유진', '영민', '정윤']
 start_date = 12
 end_date = 14
 
-for date in range(start_date, end_date+1):
-    print(f"{date}일: {random.choice(peoples)}")
+presenters = random.sample(peoples, end_date-start_date+1)
+for date, presenter in zip(range(start_date, end_date+1), presenters):
+    print(f"{date}일: {presenter}")
