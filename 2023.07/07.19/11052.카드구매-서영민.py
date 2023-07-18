6468462cfb613227335edcca08114fce0cad8535
Youngmanbo <yo900326@gmail.com>
2023-07-19 08:34:41 +0900

    '11052'

---
+++ 2023.07/07.19/11052.카드구매-서영민.py
@@ -0,0 +1,11 @@
+N = int(input())
+card_list = [0] + list(map(int, input().split()))
+
+
+dp = [0]*(N+1)
+
+for i  in range(1, N+1):
+    for j in range(1, i+1):
+        dp[i] = max(dp[i], dp[i-j]+ card_list[j])
+
+print(dp[-1])
