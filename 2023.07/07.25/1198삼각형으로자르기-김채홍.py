257daa290c4dc21437b788b493637fc6a10cd886
hoi <coghd28@naver.com>
2023-07-25 23:23:25 +0900

    1198김채홍

---
+++ 2023.07/07.25/1198삼각형으로자르기-김채홍.py
@@ -0,0 +1,13 @@
+from itertools import combinations
+import sys
+
+
+jum = []
+max_area = []
+a = []
+inp = sys.stdin.readline()
+
+for i in range(int(inp)):
+    inpp = sys.stdin.readline()
+    jum.append(list(map(int, inpp.split())))
+
