3101276844048e3da2de27ac23cd9c51a5605756
hoi <coghd28@naver.com>
2023-07-28 18:00:32 +0900

    2293동전김채홍

---
+++ 2023.07/07.26/2293동전1-김채홍.py
@@ -0,0 +1,13 @@
+n, k = map(int, input().split())
+coin = []
+for _ in range(n):
+    coin.append(int(input()))
+
+dp = [0] * (k+1)
+dp[0] = 1
+
+for i in coin:
+    for j in range(i, k+1):
+        dp[j] += dp[j - i]
+
+print(dp[k])
