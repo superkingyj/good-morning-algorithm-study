ea0a8389dc858c406ca74dbcd362e92efef5b33c
UNeedStudy <zlsoq@naver.com>
2023-08-20 09:27:15 +0900

    '문제제출'

---
+++ 2023.08/08.18/1699.제곱수의합-김정윤.py
@@ -0,0 +1,15 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+dp = [i for i in range(N + 1)]
+
+
+for i in range(1, N + 1):
+    for j in range(1, i):
+        if j * j > i:
+            break
+        if dp[i] > dp[i - j * j] + 1:
+            dp[i] = dp[i - j * j] + 1
+
+print(dp[N])
