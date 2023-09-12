9361f8a6588d7c185fd7a353c0005daca5fc9eae
woni1010011 <romoney@naver.com>
2023-09-12 18:25:43 +0900

    23.09.12 - 박성원

---
+++ 2023.09/09.12/11051.이항계수2-박성원.py
@@ -0,0 +1,18 @@
+# https://www.acmicpc.net/problem/11051
+
+import sys
+input = sys.stdin.readline
+n, k = map(int, input().split())
+d = [[0] * (n+1) for _ in range(n+1)]
+
+for i in range(0, n+1) :
+    d[i][1] = i
+    d[i][0] = 1
+    d[i][i] = 1
+
+for i in range(2, n+1) :
+    for j in range(1, i) :
+        d[i][j] = d[i-1][j] + d[i-1][j-1]
+        d[i][j] = d[i][j] % 10007
+
+print(d[n][k])
