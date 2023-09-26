f3b77e86396f1429521a9c4dbe76984f874e6d88
woni1010011 <romoney@naver.com>
2023-09-26 18:23:06 +0900

    solve

--- 2023.09/09.22/1992.쿼드트리-박성원.py
+++ 2023.09/09.22/1992.쿼드트리-박성원.py
@@ -2,7 +2,7 @@ n = int(input())
 graph = [list(map(int, input())) for _ in range(n)]
 
 
-def dnc(x, y, n):
+def dfs(x, y, n):
     check = graph[x][y]
     for i in range(x, x + n):
         for j in range(y, y + n):
@@ -13,10 +13,10 @@ def dnc(x, y, n):
     if check == -1:
         print("(", end='')
         n = n // 2
-        dnc(x, y, n)  # 오른쪽 위
-        dnc(x, y + n, n)  # 왼쪽 위
-        dnc(x + n, y, n)  # 오른쪽 아래
-        dnc(x + n, y + n, n)  # 왼쪽 아래
+        dfs(x, y, n)  # 오른쪽 위
+        dfs(x, y + n, n)  # 왼쪽 위
+        dfs(x + n, y, n)  # 오른쪽 아래
+        dfs(x + n, y + n, n)  # 왼쪽 아래
         print(")", end='')
 
     elif check == 1:
@@ -25,4 +25,4 @@ def dnc(x, y, n):
         print(0, end='')
 
 
-dnc(0, 0, n)
+dfs(0, 0, n)
