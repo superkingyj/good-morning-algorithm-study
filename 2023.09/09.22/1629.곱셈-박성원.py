bcea3e46bb29e0fbe0e313bd0b1425771cbca097
woni1010011 <romoney@naver.com>
2023-09-26 18:34:22 +0900

    solve

--- 2023.09/09.22/1629.곱셈-박성원.py
+++ 2023.09/09.22/1629.곱셈-박성원.py
@@ -1,15 +1,15 @@
-import sys
-a,b,c = map(int,sys.stdin.readline().split())
+A, B, C = map(int, input().split())
 
-def multi (a,n):
-  if n == 1:
-      return a%c
-  else:
-      tmp = multi(a,n//2)
-      if n %2 ==0:
-          return (tmp * tmp) % c
-      else:
-          return (tmp  * tmp *a) %c
-          
-print(multi(a,b))
+# 분할정복
+def solve(a, b):
+    if b == 1:  # b의 값이 1이 될 때까지 재귀
+        return a % C
+    else:
+        tmp = power(a, b // 2)  # a^(b // 2)
+        if b % 2 == 0:
+            return tmp * tmp % C  # b가 짝수인 경우
+        else:
+            return tmp * tmp * a % C  # b가 홀수인 경우
 
+res = solve(A, B)
+print(res)
